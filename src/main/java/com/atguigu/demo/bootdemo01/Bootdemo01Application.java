ec69b9041a06caab8982293a223a5328c237090b
jia <1721009302@qq.com>
2020-03-24 00:02:31 +0800

    untitled初始提交

---
+++ src/main/java/com/atguigu/demo/bootdemo01/Bootdemo01Application.java
@@ -0,0 +1,13 @@
+package com.atguigu.demo.bootdemo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootdemo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bootdemo01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/atguigu/demo/bootdemo01/Bootdemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.demo.bootdemo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bootdemo01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
